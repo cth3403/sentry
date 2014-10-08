f4c30bd8bee72f15356d9296ae8bffcdd483b74f
Joe Schulkins <jschulkins@gmail.com>
2014-10-08 14:36:26 +0100

    corrected typo of CardNo for MemberNo in SQL statement

--- kiosk/reset_user_total.php
+++ kiosk/reset_user_total.php
@@ -2,8 +2,8 @@
 
 switch ($reset_usr) {
 	case 'card_no':
-		$reset = mysqli_query($mysqli, "UPDATE members SET KioskCardCount = '0' WHERE CardNo = '".$_POST['number']."' ");
-		$newCount = mysqli_query($mysqli, "SELECT KioskCardCount FROM members WHERE CardNo = '".$_POST['number']."' ");
+		$reset = mysqli_query($mysqli, "UPDATE members SET KioskCardCount = '0' WHERE MemberNo = '".$_POST['number']."' ");
+		$newCount = mysqli_query($mysqli, "SELECT KioskCardCount FROM members WHERE MemberNo = '".$_POST['number']."' ");
 		break;
 
 	default:
